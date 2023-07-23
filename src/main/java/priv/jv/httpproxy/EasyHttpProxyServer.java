73426b45ec31ce77552e748ed1e351e8b3433dac
Javie0925 <49177582+Javie0925@users.noreply.github.com>
2023-07-23 08:06:50 +0000

    delete useless files

--- src/main/java/priv/jv/httpproxy/EasyHttpProxyServer.java
+++ src/main/java/priv/jv/httpproxy/EasyHttpProxyServer.java
@@ -4,83 +4,79 @@ package priv.jv.httpproxy;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.NettyRuntime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import priv.jv.httpproxy.handler.proxy.HttpProxyHandler;
-import priv.jv.httpproxy.handler.proxy.HttpsProxyHandler;
-import priv.jv.httpproxy.handler.proxy.SocksProxyHandler;
+import priv.jv.httpproxy.proxy.handler.HttpProxyClientHandler;
+import priv.jv.httpproxy.proxy.handler.SocksProxyHandler;
+
+import java.text.MessageFormat;
 
 
 /**
- * EasyHttpProxyServer
- * created on 2019/10/25 14:44
- *
- * @author puhaiyang
+ * HTTP/HTTPS代理服务器
+ * 三个角色：真实客户端，代理客户端，目标主机
+ * <p>
+ * 数据流向：
+ * <p>
+ * ------->> 代理客户端  --------->>
+ * 真实客户端                                  目标主机
+ * <<-------- 代理客户端  <<--------
  */
 public class EasyHttpProxyServer {
     private static Logger logger = LoggerFactory.getLogger(EasyHttpProxyServer.class);
-    private static EasyHttpProxyServer instace = new EasyHttpProxyServer();
 
-    public static EasyHttpProxyServer getInstace() {
-        if (instace == null) {
-            instace = new EasyHttpProxyServer();
-        }
-        return instace;
+    int port;
+
+    public EasyHttpProxyServer(int port) {
+        this.port = port;
     }
 
-    public static void main(String[] args) {
-        System.out.println("start proxy server");
+    public static void main(String[] args) throws Exception {
+        printWelcome();
         int port = 8888;
         if (args.length > 0) {
             port = Integer.valueOf(args[0]);
         }
-        new EasyHttpProxyServer().start(port);
+        new EasyHttpProxyServer(port).run();
+        logger.info("proxy server start on {} port", port);
     }
 
-    /**
-     * 启动
-     *
-     * @param listenPort 监控的端口
-     */
-    public void start(int listenPort) {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+    private static void printWelcome() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>> starting proxy server <<<<<<<<<<<<<<<<<<<<<<");
+        System.out.println(MessageFormat.format(">>>>>>>>>>>>>>>>>>>>>>>>>> available process: {0} <<<<<<<<<<<<<<<<<<<<<<",
+                Constans.AVAILABLE_PROCESS));
+        System.out.println();
+    }
+
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(Constans.AVAILABLE_PROCESS);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(NettyRuntime.availableProcessors() * 5);
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 100)
-                    .option(ChannelOption.TCP_NODELAY, true)
                     .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ChannelInitializer<Channel>() {
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        protected void initChannel(Channel ch) throws Exception {
-                            //ch.pipeline().addLast("httpCodec", new HttpServerCodec());
-                            //接收客户端请求，将客户端的请求内容解码
-                            ch.pipeline().addLast("httpRequestDecoder", new HttpRequestDecoder());
-                            //发送响应给客户端，并将发送内容编码
-                            ch.pipeline().addLast("httpResponseEncoder", new HttpResponseEncoder());
-                            ch.pipeline().addLast("httpAggregator", new HttpObjectAggregator(65536));
-                            ch.pipeline().addLast("httpProxyHandler", new HttpProxyHandler());
-                            ch.pipeline().addLast("httpsProxyHandler", new HttpsProxyHandler());
-                            ch.pipeline().addLast("socksProxyHandler", new SocksProxyHandler());
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    new LoggingHandler(LogLevel.WARN),
+                                    new HttpProxyClientHandler(),
+                                    new SocksProxyHandler());
                         }
-                    });
-            logger.info("[EasyHttpProxyServer] proxy server start on {} port", listenPort);
-            ChannelFuture f = b
-                    .bind(listenPort)
+                    })
+                    .bind(port)
                     .sync()
                     .channel()
                     .closeFuture()
                     .sync();
-        } catch (Exception e) {
-            logger.error("exception occurs while during starting.",e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
